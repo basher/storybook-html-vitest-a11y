5d9f76897817f69934c63795dfe2289632e2dfee
basher <basher@madasafish.com>
2026-04-22 16:46:50 +0100

    fix vitest imports

--- .storybook/vitest.setup.js
+++ .storybook/vitest.setup.js
@@ -1,5 +1,5 @@
-import * as addonA11y from '@storybook/addon-a11y/preview';
-import { setProjectAnnotations } from '@storybook/html-vite';
+import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
+import { setProjectAnnotations } from 'storybook/internal/preview-api';
 import * as projectAnnotations from './preview';
 
-setProjectAnnotations([projectAnnotations, addonA11y]);
+setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
